54b6ad868b43054e28872768c2732f46751bdb52
Triana Hoyos Ana <trianaa1@login2.triton.aalto.fi>
2019-08-26 18:31:44 +0300

    add file to run thresholded analysis in cluster

---
+++ run_slurm_permutations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+#SBATCH --time=01:00:00 
+#SBATCH --mem-per-cpu=4096
+#SBATCH -p debug
+#SBATCH -o job-%a.out
+#SBATCH --array=0-8
+module load matlab
+matlab -nojvm -r "my_slurm_permutations($SLURM_ARRAY_TASK_ID); quit"
