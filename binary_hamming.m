4883356398061b9f15fa52d2b24b7907222f1c47
Triana Hoyos Ana <trianaa1@login2.triton.aalto.fi>
2019-10-31 20:42:35 +0200

    fix colors of NaN values, add save path

--- binary_hamming.m
+++ binary_hamming.m
@@ -12,12 +12,15 @@ clc
 addpath(genpath('/m/cs/scratch/networks/trianaa1/toolboxes/BCT'));
 
 smooth={'0','4','6','8','10','12','14','16','18','20','22','24','26','28','30','32'};
-parcellation={'Brainnetome','Craddock30','Craddock100','Craddock350'};
-thr='01';
-N=[246,30,98,329];
+parcellation={'Brainnetome','Craddock100','Craddock350'};
+thr='013';
+N=[246,98,329];
+%save_path='/m/cs/scratch/networks/trianaa1/Paper1/Figures/ABIDE_extended';
+save_path='/m/cs/scratch/networks/trianaa1/Paper1/Figures/UCLA';
 
 % Distance of matrices
 for p=1:size(parcellation,2)
+    record=NaN(1,length(smooth));
     for i=1:size(smooth,2)
         fprintf('smooth:%s and parcellation:%s \n',smooth{i},parcellation{p})
         %load(sprintf('/m/cs/scratch/networks/data/ABIDE_II/Analysis/ABIDE_extended/Permutations/%s/weightedlinks_%smm_%s.mat',parcellation{p},smooth{i},thr))
@@ -33,7 +36,12 @@ for p=1:size(parcellation,2)
         links(ids) = tstats.*(pcor<0.05);
         mat{i}=links;
         
+        if all(links(:)==0)
+            record(i)=i;
+        end
+        
     end
+    record=record(~isnan(record));
     
     hamming=zeros(size(smooth,2),size(smooth,2));
     
@@ -46,15 +54,23 @@ for p=1:size(parcellation,2)
         end
     end
     hamming = hamming.*-1;
+    for i=1:length(record)
+        hamming(record(i),:)=NaN(1,16);
+        hamming(:,record(i))=NaN(16,1);
+    end
 
     %Hamming distances
     f=figure;
-    hex=['#003766';'#005399';'#1762A2';'#2E72AB';'#4581B4';'#5C91BE';'#73A1C7';'#8BB0D0';'#A2C0D9';'#B9D0E3';'#D0DFEC';'#FFFFFF']; %#E7EFF5
+    %hex=['#003766';'#005399';'#1762A2';'#2E72AB';'#4581B4';'#5C91BE';'#73A1C7';'#8BB0D0';'#A2C0D9';'#B9D0E3';'#D0DFEC';'#FFFFFF']; %#E7EFF5
+    hex=['#003766';'#1762A2';'#73A1C7';'#A2C0D9';'#D0DFEC';'#FFFFFF'];
     my_map = sscanf(hex','#%2x%2x%2x',[3,size(hex,1)]).' / 255;
     colormap(my_map)
-    heatmap(smooth,smooth,hamming,'Colormap',my_map,'FontSize',20,'XLabel','Smoothing Kernel','YLabel','Smoothing kernel','CellLabelColor','none')
+    heatmap(smooth,smooth,hamming,'Colormap',my_map,'FontSize',20,'XLabel','Smoothing Kernel','YLabel','Smoothing kernel','CellLabelColor','none','MissingDataColor','#C0C0C0')
     title(sprintf('Similarity matrix for %s',parcellation{p}))
-    %caxis([-0.001,0])
+    caxis([-0.003,0])
     set(gcf,'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
     set(gcf,'color',[1 1 1]);
+    
+    saveas(f,sprintf('%s/weight_%s_hamming.svg',save_path,parcellation{p}),'svg')
+    saveas(f,sprintf('%s/weight_%s_hamming.eps',save_path,parcellation{p}),'epsc')
 end
