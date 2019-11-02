cae1c70d654c663e77e2849a0113eafa17e0e2d4
Triana Hoyos Ana <trianaa1@login2.triton.aalto.fi>
2019-11-02 22:02:14 +0200

    clean up for submission

--- my_effectsize_test.m
+++
@@ -1,8 +0,0 @@
-clear all
-close all
-clc
-
-addpath(genpath('/m/cs/scratch/networks/trianaa1/toolboxes/MES'));
-
-load('/m/cs/scratch/networks/trianaa1/toolboxes/MES/exampleData.mat')
-effect=mes1way(com_post,'g_psi','group',group,'cWeight',[-1 1 0]);
--- my_run_ttest_permutations_craddock350.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#SBATCH -t 27:30:00
-#SBATCH -n 1
-#SBATCH --mem-per-cpu=6000
-#SBATCH -o /m/cs/scratch/networks/data/ABIDE_II/Analysis/ABIDE_extended/Permutations/Craddock350_jobs/job-extra-%a.out
-#SBATCH --array=0-32
-
-module load matlab
-matlab -nojvm -r "my_run_ttest_permutations_craddock350($SLURM_ARRAY_TASK_ID); quit"
