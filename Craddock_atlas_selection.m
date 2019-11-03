a786e3c575fabe73046626152915f6fdadcceb02
Ana Triana Hoyos <ana.trianahoyos@aalto.fi>
2019-11-03 11:07:01 +0200

    Update Craddock_atlas_selection.m

--- Craddock_atlas_selection.m
+++ Craddock_atlas_selection.m
@@ -1,3 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This script selects the craddock parcellations from the Craddock Atlas used  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clear all
 close all
 clc
@@ -11,15 +15,11 @@ y=size(nii.img,2);
 z=size(nii.img,3);
 t=size(nii.img,4);
 
-atlas1=nii.img(:,:,:,3);%select the atlas with 30 ROIs
-atlas2=nii.img(:,:,:,10);%select the atlas with 100 ROIs
-atlas3=nii.img(:,:,:,31);%select the atlas with 350 ROIs
-
-nii_path = '/m/cs/scratch/networks/trianaa1/Atlas/Craddock_random_30.nii';
-save_nii(make_nii(atlas1, [x y z]), nii_path)
+atlas1=nii.img(:,:,:,10);%select the atlas with 100 ROIs
+atlas2=nii.img(:,:,:,31);%select the atlas with 350 ROIs
 
 nii_path = '/m/cs/scratch/networks/trianaa1/Atlas/Craddock_random_100.nii';
-save_nii(make_nii(atlas2, [x y z]), nii_path)
+save_nii(make_nii(atlas1, [x y z]), nii_path)
 
 nii_path = '/m/cs/scratch/networks/trianaa1/Atlas/Craddock_random_350.nii';
-save_nii(make_nii(atlas3, [x y z]), nii_path)
+save_nii(make_nii(atlas2, [x y z]), nii_path)
